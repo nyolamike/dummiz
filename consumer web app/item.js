6e82d862608d531f405def8f7c69521ebd3abac6
nyola mike <nyolamike@live.com>
2019-09-20 11:03:15 +0300

    shooping list

---
+++ consumer web app/item.js
@@ -0,0 +1,13 @@
+$(function(){
+    var user = null;
+    user = {};
+    var indexApp = new Vue({
+        el:"#indexApp",
+        data:{
+            user: user,
+        },
+        methods:{
+
+        }
+    });
+});
